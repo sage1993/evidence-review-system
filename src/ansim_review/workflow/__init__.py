393721d70f5d477639bc4656e05d66b47d92ae53
kimsh <46098502+sage1993@users.noreply.github.com>
2026-08-04 00:37:49 +0900

    feat: expose workflow event APIs

--- src/ansim_review/workflow/__init__.py
+++ src/ansim_review/workflow/__init__.py
@@ -1,5 +1,15 @@
 """Resumable review workflow contracts and orchestration helpers."""
 
+from ansim_review.workflow.events import (
+    WorkflowEvent,
+    append_workflow_event,
+    decode_workflow_event,
+    load_workflow_events,
+    make_workflow_event,
+    project_workflow_state,
+    workflow_event_bytes,
+    workflow_event_document,
+)
 from ansim_review.workflow.request import (
     RequestAttachment,
     ReviewRequest,
@@ -11,15 +21,25 @@ from ansim_review.workflow.request import (
     review_request_document,
     review_request_sha256,
 )
+from ansim_review.workflow.state_machine import validate_transition
 
 __all__ = [
     "RequestAttachment",
     "ReviewRequest",
+    "WorkflowEvent",
+    "append_workflow_event",
     "confirmed_attachment_documents",
     "confirmed_attachments",
     "decode_review_request",
+    "decode_workflow_event",
+    "load_workflow_events",
+    "make_workflow_event",
+    "project_workflow_state",
     "requires_role_confirmation",
     "review_request_bytes",
     "review_request_document",
     "review_request_sha256",
+    "validate_transition",
+    "workflow_event_bytes",
+    "workflow_event_document",
 ]
